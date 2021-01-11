fff028021bdccb3043cc1270ec52b658a95e7860
shtesh <vshtefanuka@gmail.com>
2021-01-11 21:01:47 +0100

    Created favorites.hbs and edit-profile in private folder in views, modified index.routes.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router  = express.Router();
 
+
+/*GET homepage*/
 router.get('/', (req, res, next) => {
+    if  (req.session.currentUser) {
+        let currentUser = true;
+        res.render('index', { currentUser, name: req.session.currentUser.name });
+} else {
     res.render('index');
-    console.log(req.session);
+}
 });
+
 
 module.exports = router;
