b1fba0b4ca026f03de8fb9934670a48170091a68
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro-2.local>
2021-01-26 20:11:46 +0100

    add edit functionality, hide login / signup if you are login in navbar, add profile link in navbar

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,12 +2,11 @@ const User = require("../models/user");
 
 const getUser = async (req, res) => {
   try {
-    console.log(req.session.currentUser);
+    const userId = req.session.currentUser;
     const user = await User.findById(req.session.currentUser)
       .populate("createdPlants")
       .lean();
-    console.log("User check", user);
-    res.render("user/profile", { user });
+    res.render("user/profile", { user, userId });
   } catch (err) {
     res.send(err);
   }
@@ -15,11 +14,9 @@ const getUser = async (req, res) => {
 
 const getFavoritesPage = async (req, res) => {
   try {
-    const userFav = await User.findById(req.session.currentUser)
-      .populate("favorites")
-      .lean();
-    console.log("User favorites", userFav);
-    res.render("user/favorites", { userFav });
+    const userId = req.session.currentUser;
+    const userFav = await User.findById(userId).populate("favorites").lean();
+    res.render("user/favorites", { userFav, userId });
   } catch (err) {
     res.send(err);
   }
@@ -27,21 +24,19 @@ const getFavoritesPage = async (req, res) => {
 
 const updateFavorites = async (req, res) => {
   const { plantId } = req.params;
-  console.log("plantid", plantId);
   const userId = req.session.currentUser;
   const user = await User.findById(userId).populate("favorites");
-  console.log("user", user);
   const isFavorite = user.favorites.find(
     (plant) => String(plant._id) === String(plantId)
   );
-  console.log("is favorite", isFavorite);
+
   if (isFavorite) {
-    const updateUser = await User.findByIdAndUpdate(
+    await User.findByIdAndUpdate(
       userId,
       { $pull: { favorites: plantId } },
       { new: true }
     );
-    console.log("update user 1", updateUser);
+
     return res.redirect("/user/favorites");
   }
   const updateUser = await User.findByIdAndUpdate(
@@ -49,20 +44,18 @@ const updateFavorites = async (req, res) => {
     { $push: { favorites: plantId } },
     { new: true }
   );
-  console.log("update user 2", updateUser);
+
   return res.redirect("/user/favorites");
 };
 
 const deleteFavorites = async (req, res) => {
   const { plantId } = req.params;
-  console.log("plantid", plantId);
   const userId = req.session.currentUser;
   const user = await User.findById(userId).populate("favorites");
-  console.log("user", user);
   const isFavorite = user.favorites.find(
     (plant) => String(plant._id) === String(plantId)
   );
-  console.log("is favorite", isFavorite);
+
   if (isFavorite) {
     const updateUser = await User.findByIdAndDelete(userId, {
       $push: { favorites: plantId },
--- models/plant.js
+++ models/plant.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const PlantSchema = new Schema(
-{
-    name: String,                              
-    height: Number,                        
-    light: String,                               
-    floweringTime: String,               
+  {
+    name: String,
+    height: Number,
+    light: String,
+    floweringTime: String,
     native: [String],
-    tags: [String],                            
-    imageURL:{type:String, default:"https://bs.floristic.org/image/o/6f45044d11b80e84508547dd1d6b15ce83805e2d"}                   
+    tags: [String],
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    imageURL: {
+      type: String,
+      default:
+        "https://bs.floristic.org/image/o/6f45044d11b80e84508547dd1d6b15ce83805e2d",
     },
-    {
-        timestamps: {
-            createdAt: "created_at",
-            updatedAt: "updated_at"
-        }
-    });
-module.exports = model('Plant', PlantSchema);
-
-    
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+module.exports = model("Plant", PlantSchema);
